0ee2e0e09226e933a7537e1f6ae21e32b1397c21
Jeremy Wright <jtwright@mit.edu>
2021-03-06 14:01:38 -0500

    fast

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,101 @@
 #![feature(exclusive_range_pattern)]
 #![feature(destructuring_assignment)]
 
-use std::collections::VecDeque;
-use std::io;
+// use std::collections::VecDeque;
+// use std::io;
 mod play;
 
-fn print_deq(deq : &VecDeque<play::nodes::Move>) {
-    for mv in deq.iter() {
-        println!("{}", mv);
-    }
-}
+// fn print_deq(deq : &VecDeque<play::nodes::Move>) {
+//     for mv in deq.iter() {
+//         println!("{}", mv);
+//     }
+// }
 
-struct Game {
-    pub board: play::nodes::Node,
-    white_turn: bool,
-    zobrist_table: ([[u64; 12]; 64], (u64, u64))
-}
+// struct Game {
+//     pub board: play::nodes::Node,
+//     white_turn: bool,
+//     zobrist_table: ([[u64; 12]; 64], (u64, u64))
+// }
 
-impl Game {
-    fn get_basic_game(zobrist_table: ([[u64; 12]; 64], (u64, u64))) -> Game {
-        let mut board = play::nodes::Node::default_board();
-        board.zobrist_table = zobrist_table;
-        Game{
-            board: board,
-            white_turn: true,
-            zobrist_table: zobrist_table
-        }
-    }
+// impl Game {
+//     fn get_basic_game(zobrist_table: ([[u64; 12]; 64], (u64, u64))) -> Game {
+//         let mut board = play::nodes::Node::default_board();
+//         board.zobrist_table = zobrist_table;
+//         Game{
+//             board: board,
+//             white_turn: true,
+//             zobrist_table: zobrist_table
+//         }
+//     }
 
-    fn file_to_number(file: char) -> i32 {
-        return match file {
-            'a' => 0,
-            'b' => 1,
-            'c' => 2,
-            'd' => 3,
-            'e' => 4,
-            'f' => 5,
-            'g' => 6,
-            'h' => 7,
-            _ => -1
-        }
-    }
+//     fn file_to_number(file: char) -> i32 {
+//         return match file {
+//             'a' => 0,
+//             'b' => 1,
+//             'c' => 2,
+//             'd' => 3,
+//             'e' => 4,
+//             'f' => 5,
+//             'g' => 6,
+//             'h' => 7,
+//             _ => -1
+//         }
+//     }
 
-    fn receive_move(& mut self, mv: String) {
-        // translate
-        let move_bytes = mv.as_bytes();
-        let start = ((move_bytes[0] - b'a') as i32, (move_bytes[1] - b'1') as i32);
-        let end = ((move_bytes[2] - b'a') as i32, (move_bytes[3] - b'1') as i32);
-        let mut mv : play::nodes::Move;
-        if move_bytes.len() == 5 {
-            // pawn promotion
-            let promote_to = move_bytes[4];
-            mv = play::nodes::Move::pawn_promote_move(&self.board, start, end, promote_to);
-        } else {
-            match self.board.state.get(&start) {
-                Some(p) => {
-                    if p.0 == b'p' {
-                        if self.board.is_ep(start, end) {
-                            mv = play::nodes::Move::ep_pawn_move(&self.board, start, end);
-                        } else {
-                            mv = play::nodes::Move::pawn_move(&self.board, start, end);
-                        }
-                    } else {
-                        mv = play::nodes::Move::sliding_move(&self.board, start, end);
-                    }
-                },
-                None => panic!("no piece at location to move!")
-            };
-        }
-        self.board.do_move(&mv);
-        self.white_turn = !self.white_turn;
-    }
+//     fn receive_move(& mut self, mv: String) {
+//         // translate
+//         let move_bytes = mv.as_bytes();
+//         let start = ((move_bytes[0] - b'a') as i32, (move_bytes[1] - b'1') as i32);
+//         let end = ((move_bytes[2] - b'a') as i32, (move_bytes[3] - b'1') as i32);
+//         let mut mv : play::nodes::Move;
+//         if move_bytes.len() == 5 {
+//             // pawn promotion
+//             let promote_to = move_bytes[4];
+//             mv = play::nodes::Move::pawn_promote_move(&self.board, start, end, promote_to);
+//         } else {
+//             match self.board.state.get(&start) {
+//                 Some(p) => {
+//                     if p.0 == b'p' {
+//                         if self.board.is_ep(start, end) {
+//                             mv = play::nodes::Move::ep_pawn_move(&self.board, start, end);
+//                         } else {
+//                             mv = play::nodes::Move::pawn_move(&self.board, start, end);
+//                         }
+//                     } else {
+//                         mv = play::nodes::Move::sliding_move(&self.board, start, end);
+//                     }
+//                 },
+//                 None => panic!("no piece at location to move!")
+//             };
+//         }
+//         self.board.do_move(&mv);
+//         self.white_turn = !self.white_turn;
+//     }
 
-    unsafe fn make_move(& mut self, compute_time: u128) -> play::nodes::Move {
-        eprintln!("board state:\n{}", self.board.get_str());
-        let (best_move, val) = play::choose_move(& mut self.board, self.white_turn, compute_time, false);
-        self.board.do_move(&best_move);
-        let is_check = self.board.is_check(self.white_turn);
-        self.board.undo_move(&best_move);
-        if is_check {
-            return play::choose_move(& mut self.board, self.white_turn, compute_time, true).0;
-        }
-        eprintln!("best move is {}", best_move);
-        return best_move;
-    }
-}
+//     unsafe fn make_move(& mut self, compute_time: u128) -> play::nodes::Move {
+//         eprintln!("board state:\n{}", self.board.get_str());
+//         let (best_move, val) = play::choose_move(& mut self.board, self.white_turn, compute_time, false);
+//         self.board.do_move(&best_move);
+//         let is_check = self.board.is_check(self.white_turn);
+//         self.board.undo_move(&best_move);
+//         if is_check {
+//             return play::choose_move(& mut self.board, self.white_turn, compute_time, true).0;
+//         }
+//         eprintln!("best move is {}", best_move);
+//         return best_move;
+//     }
+// }
 
 fn main() {
     // unsafe {
     //     play()
     // }
-
-    bb_test();
+    unsafe {
+        bb_test();
+    }
 }
 
-fn bb_test() {
+unsafe fn bb_test() {
     let nm = play::bb::BB::gen_knight_mask();
     let rm = play::bb::BB::gen_rook_mask();
     let bm = play::bb::BB::gen_bishop_mask();
@@ -141,6 +142,21 @@ fn bb_test() {
     for mv in qm.drain(0..) {
         println!("{}", mv);
     }
+
+    // let mut nm = db.knight_moves(true);
+    // println!("knight moves");
+    // for mv in nm.drain(0..) {
+    //     db.do_move(&mv);
+    //     let mut nm = db.knight_moves(true);
+    //     db.undo_move(&mv);
+    //     for mv in nm.drain(0..) {
+    //         println!("{}", mv);
+    //     }
+    //     break;
+    // }
+
+    let (mv, val) = play::best_move(&mut db, true, 10000);
+    println!("best move {} val {}", mv, val);
 }
 
 fn get_calc_time(time: i32) -> u128 {
@@ -158,75 +174,75 @@ fn get_calc_time(time: i32) -> u128 {
     return calc_time as u128;
 }
 
-unsafe fn play() {
-    let zobrist = play::nodes::Node::init_zobrist();
-    let mut game : Game = Game::get_basic_game(zobrist);
-    loop {
-        let mut inp : String = String::new();
-        io::stdin()
-            .read_line(&mut inp)
-            .expect("Failed to read line");
+// unsafe fn play() {
+//     let zobrist = play::nodes::Node::init_zobrist();
+//     let mut game : Game = Game::get_basic_game(zobrist);
+//     loop {
+//         let mut inp : String = String::new();
+//         io::stdin()
+//             .read_line(&mut inp)
+//             .expect("Failed to read line");
 
-        let mut params = inp.trim().split_whitespace();
-        let cmd = match params.next() {
-            Some(p) => p,
-            None => {continue;}
-        };
-        // println!("CMD {}", cmd);
-        if cmd == "quit" {
-            break;
-        }
-        if cmd == "uci" {
-            println!("id name Mantissa");
-            println!("id author jtwright");
-            println!("uciok");
-        }
-        if cmd == "isready" {
-            println!("readyok");
-        }
-        if cmd == "setoption" {}
-        if cmd == "ucinewgame" {
-            game = Game::get_basic_game(zobrist);
-        }
-        if cmd == "position" {
-            match params.next() {
-                Some(p) => {
-                    if p == "startpos" {
-                        game = Game::get_basic_game(zobrist);
-                        if params.next() == Some("moves") {
-                            loop {
-                                match params.next() {
-                                    Some(mv) => {game.receive_move(mv.to_string());},
-                                    None => {break;}
-                                };
-                            }
-                        }
-                    }
-                },
-                None => { continue; }
-            };
-        }
-        if cmd == "go" {
-            let clock_key = if game.board.white_turn {"wtime"} else {"btime"};
-            let mut time = 10000;
-            loop {
-                let p = match params.next() {
-                    Some(p) => p,
-                    None => { break; }
-                };
-                if p == clock_key {
-                    let clock_time = match params.next() {
-                        Some(p) => match p.trim().parse() {
-                            Ok(num) => num,
-                            Err(_) => panic!("error in time parse")
-                        },
-                        None => panic!("empty time!")
-                    };
-                    time = get_calc_time(clock_time);
-                }
-            }
-            let mv = game.make_move(time);
-            println!("bestmove {}", mv);
-        }
-    }
-}
+//         let mut params = inp.trim().split_whitespace();
+//         let cmd = match params.next() {
+//             Some(p) => p,
+//             None => {continue;}
+//         };
+//         // println!("CMD {}", cmd);
+//         if cmd == "quit" {
+//             break;
+//         }
+//         if cmd == "uci" {
+//             println!("id name Mantissa");
+//             println!("id author jtwright");
+//             println!("uciok");
+//         }
+//         if cmd == "isready" {
+//             println!("readyok");
+//         }
+//         if cmd == "setoption" {}
+//         if cmd == "ucinewgame" {
+//             game = Game::get_basic_game(zobrist);
+//         }
+//         if cmd == "position" {
+//             match params.next() {
+//                 Some(p) => {
+//                     if p == "startpos" {
+//                         game = Game::get_basic_game(zobrist);
+//                         if params.next() == Some("moves") {
+//                             loop {
+//                                 match params.next() {
+//                                     Some(mv) => {game.receive_move(mv.to_string());},
+//                                     None => {break;}
+//                                 };
+//                             }
+//                         }
+//                     }
+//                 },
+//                 None => { continue; }
+//             };
+//         }
+//         if cmd == "go" {
+//             let clock_key = if game.board.white_turn {"wtime"} else {"btime"};
+//             let mut time = 10000;
+//             loop {
+//                 let p = match params.next() {
+//                     Some(p) => p,
+//                     None => { break; }
+//                 };
+//                 if p == clock_key {
+//                     let clock_time = match params.next() {
+//                         Some(p) => match p.trim().parse() {
+//                             Ok(num) => num,
+//                             Err(_) => panic!("error in time parse")
+//                         },
+//                         None => panic!("empty time!")
+//                     };
+//                     time = get_calc_time(clock_time);
+//                 }
+//             }
+//             let mv = game.make_move(time);
+//             println!("bestmove {}", mv);
+//         }
+//     }
+// }
--- src/play/mod.rs
+++ src/play/mod.rs
@@ -1,92 +1,81 @@
 use std::cmp;
-use std::collections::HashMap;
 use std::time::SystemTime;
 
-pub mod nodes;
 pub mod bb;
-use nodes::Node;
-use nodes::Move;
 use bb::BB;
+use bb::Mv;
 
 static mut evaled: u64 = 0;
 
-pub unsafe fn choose_move(node: &mut Node, maximize: bool, compute_time: u128, check_check: bool) -> (Move, f64) {
-    let mut m_depth = 3;
-    let mut q_depth = 4;
+const LB: i32 = -10000000;
+const UB: i32 = 10000000;
 
-    let start_time = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+const PV_NODE: u8 = 1;
+const CUT_NODE: u8 = 2;
+const ALL_NODE: u8 = 3;
+
+pub fn get_time_millis() -> u128 {
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
         Ok(n) => n.as_millis(),
         Err(_) => panic!("SystemTime failed!"),
-    };
+    }
+}
+
+pub unsafe fn best_move(node: &mut BB, maximize: bool, compute_time: u128) -> (Mv, f64) {
+    let mut m_depth = 3;
+    let mut q_depth = 6;
+
+    let start_time = get_time_millis();
     let mut current_time = start_time;
-    let mut best_move : Move = Move::null_move();
-    let mut val = if maximize {-200000} else {200000};
-    let mut z_table : HashMap<u64, (Move, i32, i32)> = HashMap::new();
-    let mut aspire: bool = false;
+    let mut best_move : Mv = Mv::null_move();
+    let mut best_val = 0;
+    let mut aspire = false;
 
     while (current_time - start_time) <= compute_time {
-        evaled = 0;
-        let mut alpha = -1000000;
-        let mut beta = 1000000;
+        let mut alpha = LB;
+        let mut beta = UB;
         if aspire {
-            alpha = val - 250;
-            beta = val + 250;
+            alpha = best_val - 250;
+            beta = best_val + 250;
         }
-        eprintln!("alpha {} beta {}", alpha, beta);
-        let res = minimax_search(node, start_time, compute_time, m_depth, q_depth, alpha, beta, maximize, & mut z_table, check_check);
-        if res.0.is_err {
-            // timeout
-            break;
-        }
-        if res.0.is_null {
-            // null move
-            aspire = false;
-            continue;
-        }
-        if aspire && (res.1 <= alpha || res.1 >= beta) {
-            eprintln!("aspiration violated");
-            aspire = false;
-            continue;
-        }
-        (best_move, val) = res;
-        eprintln!("{} eval'd {} this time got val {}", m_depth, evaled, val);
-        current_time = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-            Ok(n) => n.as_millis(),
-            Err(_) => panic!("SystemTime failed!"),
-        };
+        let (ply_move, ply_val, node_type) = negamax_search(
+            node,
+            start_time,
+            compute_time,
+            m_depth,
+            q_depth,
+            alpha,
+            beta,
+            maximize
+        );
+
+        if ply_move.is_err {break;}
+        if ply_move.is_null || node_type != PV_NODE {aspire = false; continue;}
+
+        (best_move, best_val) = (ply_move, ply_val);
+        eprintln!("{} eval'd {} this time got val {}", m_depth, evaled, best_val);
+        current_time = get_time_millis();
         aspire = true;
         m_depth += 1;
     }
-    eprintln!("{}", m_depth);
-    eprintln!("eval'd {}", evaled);
-    eprintln!("projected: {}", val);
-    return (best_move, val as f64 / 1000.);
+    eprintln!("{} ply evaluated", m_depth - 1);
+    eprintln!("{} nodes evaluated", evaled);
+    eprintln!("projected value: {}", best_val);
+    eprintln!("elapsed time: {}", current_time - start_time);
+    return (best_move, best_val as f64 / 1000.);
 }
 
-fn is_terminal(node: &Node) -> bool {
-    return node.material.abs() >= 100000 || node.is_threefold();
+fn is_terminal(node: &BB) -> bool {
+    // TODO better terminal
+    return node.material.abs() >= 100000;
 }
 
-unsafe fn evaluate_position(node: &Node) -> i32 {
-    if node.is_threefold() {
-        return 0;
-    }
+unsafe fn evaluate_position(node: &BB) -> i32 {
     evaled += 1;
-    let mut val = node.material;
-    let (mob, kdv) = node.mobility_value();
-    val += mob / 10;
-    val += node.center_value() / 3;
-    val -= node.doubled_pawns_value() / 2;
-    val -= node.isolated_pawns_value() / 2;
-    val -= node.backwards_pawns_value() / 4;
-    val -= kdv;
-    val += node.piece_synergy_values();
-
-    val += if node.white_turn {200} else {-200};
-    return val;
+    return node.material * if node.white_turn {1} else {-1};
 }
 
-fn is_quiet(node: &mut Node) -> bool {
+fn is_quiet(node: &mut BB) -> bool {
     let mut loud = false;
     match node.cap_stack.pop() {
         Some(cap) => {
@@ -98,164 +87,88 @@ fn is_quiet(node: &mut Node) -> bool {
     return !loud;
 }
 
-unsafe fn minimax_search(node: &mut Node, start_time: u128, compute_time: u128, depth: i32, q_depth: i32, alpha: i32, beta: i32, maximize: bool, table: & mut HashMap<u64, (Move, i32, i32)>, check_check: bool) -> (Move, i32) {
-    // check time
-    let current_time = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-            Ok(n) => n.as_millis(),
-            Err(_) => panic!("SystemTime failed!"),
-    };
-    if current_time - start_time > compute_time {return (Move::err_move(), 0)}
+unsafe fn negamax_search(node: &mut BB, start_time: u128, compute_time: u128, depth: i32,
+                         q_depth: i32, alpha: i32, beta: i32, maximize: bool) -> (Mv, i32, u8) {
+    let current_time = get_time_millis();
+    if current_time - start_time > compute_time { return (Mv::err_move(), 0, PV_NODE); }
+
     let mut alpha = alpha;
     let mut beta = beta;
-    let mut first_move = nodes::Move::null_move();
-    // check cache
-    match table.get(&node.get_hash()) {
-        Some(p) => {
-            if !p.0.is_null && !p.0.is_err {
-                let mv = nodes::Move::copy_move(&p.0);
-                if p.2 >= depth { return (mv, p.1) }
-                else {first_move = mv}
-            }
-        }
-        None => {}
-    }
 
     if is_terminal(&node) {
-        return (nodes::Move::null_move(), evaluate_position(&node));
+        return (Mv::null_move(), evaluate_position(&node), PV_NODE);
     }
 
     if depth == 0 {
-        if q_depth == 0 || is_quiet(node) {
+        if is_quiet(node) {
             let val = evaluate_position(&node);
-            return (nodes::Move::null_move(), val);
+            return (Mv::null_move(), val, PV_NODE);
         } else {
-            return (nodes::Move::null_move(), quiescence_search(node, q_depth, alpha, beta, maximize));
+            let (val, node_type) = quiescence_search(node, q_depth, alpha, beta, maximize);
+            return (Mv::null_move(), val, node_type);
         }
     }
 
-    let mut best_move = nodes::Move::null_move();
-    let mut val = if maximize {-1000000} else {1000000};
-    let mut moves_to_assess = node.moves();
-    let mut best_move_guess = false;
-    let mut first = false;
-    if !first_move.is_null {
-        if moves_to_assess.contains(&first_move) {
-            best_move_guess = true;
-            first = true;
-            moves_to_assess.push_front(first_move);
+    let mut best_move = Mv::null_move();
+    let mut val = LB;
+    let mut moves = node.moves();
+    for mv in moves.drain(0..) {
+        node.do_move(&mv);
+        let res = negamax_search(node, start_time, compute_time, depth - 1, q_depth, -beta, -alpha, !maximize);
+        node.undo_move(&mv);
+        let ret_move = res.0;
+        if ret_move.is_err {
+            return (Mv::err_move(), 0, PV_NODE);
         }
-    }
+        let ret_val = -res.1;
+        let ret_node_type = res.2;
+        if ret_val > val {
+            best_move = mv;
+            val = ret_val;
+        }
+        alpha = cmp::max(alpha, val);
 
-    if moves_to_assess.len() == 0 {
-        if node.is_check(maximize) {
-            return (best_move, if maximize {node.material - 200000} else {node.material + 200000});
-        }
-        return (best_move, 0);
-    }
-    for pot_move in moves_to_assess.drain(0..) {
-        node.do_move(&pot_move);
-        let mut mv : Move;
-        let mut new_val: i32;
-        if !best_move_guess || first {
-            (mv, new_val) = minimax_search(node, start_time, compute_time, depth - 1, q_depth, alpha, beta, !maximize, table, check_check);
-        } else {
-            if maximize {
-                (mv, new_val) = minimax_search(node, start_time, compute_time, depth - 1, q_depth, alpha, alpha + 1, !maximize, table, check_check);
-            } else {
-                (mv, new_val) = minimax_search(node, start_time, compute_time, depth - 1, q_depth, beta - 1, beta, !maximize, table, check_check);
-            }
-
-            if !mv.is_err && (alpha < new_val && beta > new_val) {
-                (mv, new_val) = minimax_search(node, start_time, compute_time, depth - 1, q_depth, alpha, beta, !maximize, table, check_check);
-            }
-        }
-        node.undo_move(&pot_move);
-        first = false;
-        if mv.is_err { return (mv, 0) }
-        if maximize {
-            if new_val > val || best_move.is_null {
-                if val.abs() >= 100000 || check_check {
-                    node.do_move(&pot_move);
-                    if node.is_check(maximize) {
-                        node.undo_move(&pot_move);
-                        continue;
-                    }
-                    node.undo_move(&pot_move);
-                }
-                best_move = pot_move;
-                val = new_val;
-            }
-            alpha = cmp::max(alpha, val);
-        } else {
-            if new_val < val || best_move.is_null {
-                if val.abs() >= 100000 || check_check {
-                    node.do_move(&pot_move);
-                    if node.is_check(maximize) {
-                        node.undo_move(&pot_move);
-                        continue;
-                    }
-                    node.undo_move(&pot_move);
-                }
-                best_move = pot_move;
-                val = new_val;
-            }
-            beta = cmp::min(beta, val);
-        }
         if alpha >= beta {
-            if maximize {
-                return (best_move, beta);
-            } else {
-                return (best_move, alpha);
-            }
+            // beta cutoff
+            return (best_move, beta, CUT_NODE);
         }
-    }
-    if best_move.is_null {
-        if node.is_check(maximize) {
-            return (best_move, if maximize {node.material - 200000} else {node.material + 200000});
-        }
-        return (best_move, 0);
     }
 
-    table.insert(node.get_hash(), (nodes::Move::copy_move(&best_move), val, depth));
-    return (best_move, val);
+    return (best_move, val, if val < alpha {ALL_NODE} else {PV_NODE});
 }
 
-unsafe fn quiescence_search(node: &mut Node, depth: i32, alpha: i32, beta: i32, maximize: bool) -> i32 {
+unsafe fn quiescence_search(node: &mut BB, depth: i32, alpha: i32, beta: i32, maximize: bool) -> (i32, u8) {
     if depth == 0 || is_terminal(node) || is_quiet(node) {
-        return evaluate_position(node);
+        return (evaluate_position(node), PV_NODE);
     }
 
     let mut alpha = alpha;
     let mut beta = beta;
-    let standing_pat = evaluate_position(node);
+    let mut raised_alpha = false;
+    let curr_val = evaluate_position(node);
+    if curr_val >= beta {
+        // beta cutoff
+        return (beta, CUT_NODE);
+    }
+    if curr_val > alpha {
+        raised_alpha = true;
+        alpha = curr_val;
+    }
 
-    if maximize {
-        if standing_pat >= beta {
-            return beta;
+    for mv in node.moves().drain(0..) {
+        node.do_move(&mv);
+        let res = quiescence_search(node, depth - 1, -beta, -alpha, !maximize);
+        node.undo_move(&mv);
+        let val = -res.0;
+
+        if val >= beta {
+            return (beta, CUT_NODE);
         }
-        if standing_pat > alpha {
-            alpha = standing_pat;
-        }
-    } else {
-        if standing_pat <= alpha {
-            return alpha;
-        }
-        if standing_pat < beta {
-            beta = standing_pat;
+        if val > alpha {
+            raised_alpha = true;
+            alpha = val;
         }
     }
 
-    for pot_move in node.moves().drain(0..) {
-        node.do_move(&pot_move);
-        let val = quiescence_search(node, depth - 1, alpha, beta, !maximize);
-        node.undo_move(&pot_move);
-        if maximize {
-            if val >= beta { return beta; }
-            if val > alpha { alpha = val; }
-        } else {
-            if val <= alpha { return alpha; }
-            if val < beta { beta = val; }
-        }
-    }
-    return if maximize {alpha} else {beta};
+    return (alpha, if raised_alpha {PV_NODE} else {ALL_NODE});
 }
