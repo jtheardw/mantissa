4e798bbd3345003355bb5ae2d6f2a996ce6896af
Jeremy Wright <jtwright@alum.mit.edu>
2021-09-18 18:03:06 -0400

    clean up imports

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,34 @@
 #![allow(unreachable_code)]
 #![feature(exclusive_range_pattern)]
 
-mod bitboard; use crate::bitboard::*;
-mod eval; use crate::eval::*;
-mod magic; use crate::magic::*;
-mod movegen; use crate::movegen::*;
-mod moveorder; use crate::moveorder::*;
-mod moveutil; use crate::moveutil::*;
-mod perft; use crate::perft::*;
-mod pht; use crate::pht::*;
-mod psqt; use crate::psqt::*;
+mod bitboard;
+mod eval;
+mod magic;
+mod movegen;
+mod moveorder;
+mod moveutil;
+mod perft;
+mod pht;
+mod psqt;
 mod rand;
-mod search; use crate::search::*;
-mod searchutil; use crate::searchutil::*;
-mod see; use crate::see::*;
+mod search;
+mod searchutil;
+mod see;
 mod tuning_eval;
 mod tuning_psqt;
-mod tuning; use crate::tuning::*;
-mod time; use crate::time::*;
-mod tt; use crate::tt::*;
-mod uci; use crate::uci::*;
-mod util; use crate::util::*;
-mod zobrist; use crate::zobrist::*;
+mod tuning;
+mod time;
+mod tt;
+mod uci;
+mod util;
+mod zobrist;
+
+use crate::magic::*;
+use crate::movegen::*;
+use crate::pht::*;
+use crate::tt::*;
+use crate::uci::*;
+use crate::util::*;
 
 fn init() {
     initialize_masks();
