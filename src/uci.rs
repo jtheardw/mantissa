711594fd5d28afc0f9c506dace42f03689e1207f
Jeremy Wright <jtwright@alum.mit.edu>
2022-01-20 08:37:55 -0500

    set upper limit to 65536 for hash and update version num

--- src/uci.rs
+++ src/uci.rs
@@ -265,7 +265,7 @@ pub fn uci_loop() {
         if cmd == "quit" {
             break;
         } else if cmd == "uci" {
-            println!("id name Mantissa v3.3.0-tcec-0");
+            println!("id name Mantissa v3.3.0-tcec-1");
             println!("id author jtwright");
             println!("option name Hash type spin default 64 min 1 max 65536");
             println!("option name Threads type spin default 1 min 1 max 256");
